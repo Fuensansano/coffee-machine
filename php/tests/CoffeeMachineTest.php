66eed339999c41d9ac85c8e001a56bce253c6bb6
Fuen Sansano <fuensansano@gmail.com>
2024-07-16 19:32:24 +0200

    refactor test class: created setUp method

--- php/tests/CoffeeMachineTest.php
+++ php/tests/CoffeeMachineTest.php
@@ -10,39 +10,45 @@ use PHPUnit\Framework\TestCase;
 
 class CoffeeMachineTest extends TestCase
 {
+    private DrinkMaker $drinkMaker;
+    private CoffeeMachine $coffeeMachine;
+
+    protected function setUp(): void
+    {
+        $this->drinkMaker = self::createMock(DrinkMaker::class);
+
+        $this->coffeeMachine = new CoffeeMachine($this->drinkMaker);
+    }
+
     /** @test */
     public function given_a_chocolate_order_then_the_machine_prepare_the_order(): void
     {
-        $drinkMaker = self::createMock(DrinkMaker::class);
+
         $order = new Order(Drink::Chocolate);
 
-        $coffeeMachine = new CoffeeMachine($drinkMaker);
+        $this->drinkMaker->expects(self::once())->method('prepare')->with("H::");
 
-        $drinkMaker->expects(self::once())->method('prepare')->with("H::");
-        $coffeeMachine->prepare($order);
+        $this->coffeeMachine->prepare($order);
     }
 
     /** @test */
     public function given_a_coffee_order_then_the_machine_prepare_the_order(): void
     {
-        $drinkMaker = self::createMock(DrinkMaker::class);
         $order = new Order(Drink::Coffee);
 
-        $coffeeMachine = new CoffeeMachine($drinkMaker);
 
-        $drinkMaker->expects(self::once())->method('prepare')->with("C::");
-        $coffeeMachine->prepare($order);
+        $this->drinkMaker->expects(self::once())->method('prepare')->with("C::");
+
+        $this->coffeeMachine->prepare($order);
     }
 
     /** @test */
     public function given_a_tea_order_then_the_machine_prepare_the_order(): void
     {
-        $drinkMaker = self::createMock(DrinkMaker::class);
         $order = new Order(Drink::Tea);
 
-        $coffeeMachine = new CoffeeMachine($drinkMaker);
 
-        $drinkMaker->expects(self::once())->method('prepare')->with("T::");
-        $coffeeMachine->prepare($order);
+        $this->drinkMaker->expects(self::once())->method('prepare')->with("T::");
+        $this->coffeeMachine->prepare($order);
     }
 }
